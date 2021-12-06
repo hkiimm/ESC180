c42e19dbafe35f2a4c033791bf293eb5e0863ea9
Hannah Kim <hannah.seyun.kim@gmail.com>
2021-12-06 16:48:34 -0500

    New Changes

--- Projects/Project 3/synonyms.py
+++ Projects/Project 3/synonyms.py
@@ -54,8 +54,9 @@ def build_semantic_descriptors_from_files(filenames):
     sem_descript = {}
 
     for i in range (len(filenames)):
-        text += open(filenames[i], "r", encoding="latin1").read().lower()
-        text = text.replace(",", "").replace("-", " ").replace("--", " ").replace(":","").replace(";", "")
+        a = "C:/Users/hannahkim/Desktop/ESC180/ESC180/Projects/Project 3/" + filenames[i]
+        text = open(a, "r", encoding="latin1").read().lower()
+        text = text.replace(",", "").replace("--", " ").replace("-", " ").replace(":","").replace(";", "")
         text = text.replace("!", ".").replace("?", ".").split(".")
 
     for i in range (len(text)):
@@ -65,9 +66,48 @@ def build_semantic_descriptors_from_files(filenames):
 
     return sem_descript
 
+
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
-    pass
+    choice_similarity = [0]*len(choices)
+    ind = 0
+
+    if word in semantic_descriptors.keys():
+        for i in range (len(choices)):
+            if choices[i] in semantic_descriptors.keys():
+                #check this
+                choice_similarity[i] = similarity_fn(semantic_descriptors[word],semantic_descriptors[choices[i]])
+            else:
+                choice_similarity[i] = -1
+    else:
+        return 0
+
+    max_sim = 0
+
+    for i in range (len(choice_similarity)):
+        if choice_similarity[i] > max_sim:
+            ind = i
+            max_sim = choice_similarity[i]
+
+    return ind
 
 
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
-    pass
+    #f = open(filename, encoding="latin1")
+    #s = f.read().split("\n")
+    s = open("C:/Users/hannahkim/Desktop/ESC180/ESC180/Projects/Project 3/test.txt", encoding="latin-1").read().split("\n")
+    correct = 0
+
+    for i in range (len(s)):
+        line = s[i].split(" ")
+        line2 = line[2:]
+        if line2[most_similar_word(line[0], line2, semantic_descriptors, similarity_fn)] == line[1]:
+            correct += 1
+
+    return float(correct/len(s))
+    
+
+if __name__ == "__main__":
+    #print (cosine_similarity({"a": 1, "b": 2, "c": 3}, {"b": 4, "c": 5, "d": 6}))
+    sem_descriptors = build_semantic_descriptors_from_files(["wp.txt", "sw.txt"])
+    res = run_similarity_test("test.txt", sem_descriptors, cosine_similarity)
+    print(res, "of the guesses were correct")
